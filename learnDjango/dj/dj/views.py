f036af92d5ce7ffae58c4badfd95e1cbb27ab989
priyapahwa <77075449+priyapahwa@users.noreply.github.com>
2021-07-10 23:16:09 +0530

    Updates

--- learnDjango/dj/dj/views.py
+++ learnDjango/dj/dj/views.py
@@ -13,4 +13,30 @@ def index(request):
 
 
 def about(request):
-    return HttpResponse("Hey")
+    # GET.get returns text in input area or form otherwise takes default
+    djtext = (request.GET.get('text', 'default'))
+
+    # check checkbox values
+    removepunc = (request.GET.get('removepunc', 'off'))
+    capitalize = (request.GET.get('capitalize', 'off'))
+    newlinerem = (request.GET.get('newlinerem', 'off'))
+
+    # to check which checkbox is on
+    if removepunc == "on":
+        punctuations = '''!()-{}[];:'"\,<>/.?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+        return render(request, 'about.html', params)
+
+    elif capitalize == "on":
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+        params = {'purpose': 'UPPERCASE', 'analyzed_text': analyzed}
+        return render(request, 'about.html', params)
+
+    else:
+        return HttpResponse("Error")
